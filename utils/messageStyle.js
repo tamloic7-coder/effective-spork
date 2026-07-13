fd8aa4623899e2199e23c30ab3a1dcbf37e18acd
tamloic7-coder <tamloic7@gmail.com>
2026-07-13 02:09:15 +0100

    Create messageStyle.js

---
+++ utils/messageStyle.js
@@ -0,0 +1,18 @@
+import fs from "fs"
+import stylizedChar from "./fancy.js"
+
+export default function stylizedCardMessage(text) {
+  return {
+    text: stylizedChar(text),
+    contextInfo: {
+      externalAdReply: {
+        title: "Digix bot",
+        body: "𓆩 Loïc 𝐂𝐫𝐞𝐰 𝟐𝟒𝟑 𓆪",
+        thumbnail: fs.readFileSync("./database/DigiX.jpg"),
+        sourceUrl: "https://whatsapp.com",
+        mediaType: 1,
+        renderLargerThumbnail: false
+      }
+    }
+  }
+}
